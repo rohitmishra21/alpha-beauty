f13b96709b6bcfb99f8c9522ca44e0ab62f50d7b
rohit mishra <mishrarohit27112005@gmail.com>
2024-06-26 10:49:51 +0530

    UI is complete

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 2.5,
     spaceBetween: 30,
     freeMode: true,
+    loop: true,
     autoplay: {
-        delay: 9000,
+        delay: 1000,
         disableOnInteraction: false,
         
       },
@@ -14,4 +15,24 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
-  swiper()
+function two() {
+  
+  var swiper2 = new Swiper(".mySwiper2", {
+     slidesPerView: 5.5,
+    spaceBetween: 0,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+      
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  
+}
+  two()
